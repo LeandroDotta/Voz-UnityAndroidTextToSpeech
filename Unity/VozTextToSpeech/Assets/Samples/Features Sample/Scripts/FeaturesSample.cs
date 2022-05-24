1270847c815aee23f4879580de7fb4ac0a085846
Leandro Dotta <leandrodotta@gmail.com>
2022-05-23 21:02:34 -0300

    (unity): improve features scene with animations

--- Unity/VozTextToSpeech/Assets/Samples/Features Sample/Scripts/FeaturesSample.cs
+++ Unity/VozTextToSpeech/Assets/Samples/Features Sample/Scripts/FeaturesSample.cs
@@ -6,47 +6,69 @@ using UnityEngine.UI;
 
 public class FeaturesSample : MonoBehaviour
 {
-    private const string STATUS_UNAVAILABLE = "Unavailable";
-    private const string STATUS_INITIALIZING = "Initializing";
-    private const string STATUS_OK = "OK";
-    private const string STATUS_SPEAKING = "Speaking";
-    private const string STATUS_SPEAK_DONE = "Done Speaking";
+    private const int STATUS_UNAVAILABLE = 0;
+    private const int STATUS_OK = 1;
+    private const int STATUS_SPEAKING = 2;
+    private const string TEXT_UNAVAILABLE = "Unavailable";
+    private const string TEXT_INITIALIZING = "Initializing";
+    private const string TEXT_OK = "OK";
+    private const string TEXT_SPEAKING = "Speaking";
+    private const string TEXT_SPEAK_DONE = "Done Speaking";
+
 
     [SerializeField] private Voz voz;
 
     [SerializeField] private Button buttonStatus;
     [SerializeField] private Text textButtonStatus;
+    [SerializeField] private Image imageButtonStatus;
     [SerializeField] private Button buttonSpeakNow;
     [SerializeField] private InputField inputSpeak;
+    [SerializeField] private Animator animButton;
+
+    [Header("Colors")]
+    [SerializeField] private Color colorUnavailable = Color.gray;
+    [SerializeField] private Color colorOK = Color.blue;
+    [SerializeField] private Color colorSpeaking = Color.yellow;
     
     private void Start() 
     {
-        textButtonStatus.text = STATUS_INITIALIZING;
+        textButtonStatus.text = TEXT_INITIALIZING;
+        imageButtonStatus.color = colorUnavailable;
     }
 
     private void OnInit()
     {
-        textButtonStatus.text = STATUS_OK;
+        textButtonStatus.text = TEXT_OK;
+        imageButtonStatus.color = colorOK;
+        animButton.SetInteger("status", STATUS_OK);
     }
 
     private void OnError()
     {
-        textButtonStatus.text = STATUS_UNAVAILABLE;
+        textButtonStatus.text = TEXT_UNAVAILABLE;
+        imageButtonStatus.color = colorUnavailable;
+        animButton.SetInteger("status", STATUS_UNAVAILABLE);
     }
 
     private void OnSpeechStart()
     {
-        textButtonStatus.text = STATUS_SPEAKING;
+        textButtonStatus.text = TEXT_SPEAKING;
+        imageButtonStatus.color = colorSpeaking;
+        animButton.SetInteger("status", STATUS_SPEAKING);
     }
 
     private void OnSpeechDone()
     {
-        textButtonStatus.text = STATUS_SPEAK_DONE;
+        textButtonStatus.text = TEXT_SPEAK_DONE;
+        imageButtonStatus.color = colorOK;
+        animButton.SetInteger("status", STATUS_OK);
     }
 
     private void OnSpeechStop()
     {
-        textButtonStatus.text = STATUS_SPEAK_DONE;
+        textButtonStatus.text = TEXT_SPEAK_DONE;
+        imageButtonStatus.color = colorOK;
+        animButton.SetInteger("status", STATUS_OK);
     }
 
     public void Speak(string text)
